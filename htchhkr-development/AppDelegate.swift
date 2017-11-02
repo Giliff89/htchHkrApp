18357195f79b01771f3a8f253cac6514cf6a7dcd
Gina Iliff <giliff@me.com>
2017-11-02 16:26:23 -0700

    Set up driver pickup mode

--- htchhkr-development/AppDelegate.swift
+++ htchhkr-development/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     fileprivate var containerVC = ContainerViewController()
     
+    var MenuContainerVC: ContainerViewController {
+        return containerVC
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
@@ -50,6 +54,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    class func getAppDelegate() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
 
 }
 
